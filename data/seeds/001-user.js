6b3044afdd5453ffb53d43e8d66c4248a4dbae4d
novinary <novina.dev@gmail.com>
2019-08-21 20:33:34 +0100

    Ran seeding scripts for users

--- data/seeds/001-user.js
+++ data/seeds/001-user.js
@@ -1,13 +1,9 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = function(knex, Promise) {
+  // the 00-cleanup.js seed already deleted all records
+  // we just worry about seeding records in all other seeds
+  // Inserts seed entries
+  return knex('users').insert([
+    { username: 'Omer', password: 'test', email: 'omer@test.com' },
+    { username: 'Novina', password: 'test', email: 'novina@test.com' }
+  ]);
 };
